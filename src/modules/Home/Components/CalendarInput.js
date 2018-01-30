f9107e84a3b58a6bad65c91d5ac55851c34a69ef
Pyro <pyrolovesbuns@gmail.com>
2018-01-30 16:01:54 +0200

    Added basic validation

--- src/modules/Home/Components/CalendarInput.js
+++ src/modules/Home/Components/CalendarInput.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Form, Field } from 'react-final-form'
+import { FORM_ERROR } from 'final-form'
 import renderInput from '../../Shared/Forms/input'
 
+const composeValidators = (...validators) => value =>
+  validators.reduce((error, validator) => error || validator(value), undefined)
+const required = value => (value ? undefined : 'Required')
+const mustBeNumber = value => (isNaN(value) ? 'Must be a number' : undefined)
+
+const Fields = [
+  {name: 'event', label: 'Event', validate: required},
+  {name: 'timeFrom', label: 'Time From', validate: composeValidators(required, mustBeNumber)},
+  {name: 'timeTo', label: 'Time To', validate: composeValidators(required, mustBeNumber)},
+]
 class CalendarForm extends Component {
   submit = (values) => {
-    alert(JSON.stringify(values))
+    const {timeFrom, timeTo, event} = values
+    if (timeTo < timeFrom) {
+      return {
+        [FORM_ERROR]: `Validation error: Time To can be bigger than Time From`
+      }
+    }
+    else {
+      alert(JSON.stringify(values))
+    }
   }
 
-  validate = (values) => {
-
-  }
-
-  renderForm = ({handleSubmit, reset, submitting, pristine, values}) => (
+  renderForm = ({handleSubmit, reset, submitting, pristine, values, submitError}) => (
     <form onSubmit={handleSubmit}>
-      <Field label="Text" name="text">
-        {renderInput}
-      </Field>
+      {Fields.map(field => (
+        <Field validate={field.validate} key={'field_' + field.name} label={field.label} name={field.name}>
+          {renderInput}
+        </Field>
+      ))}
+      <button> Submit</button>
+      {submitError && <SubmitError>{submitError}</SubmitError>}
     </form> )
 
   render () {
     return (
       <Wrapper>
+        <h2>Add new event</h2>
         <Form onSubmit={this.submit}
               render={this.renderForm}
         />
@@ -32,4 +52,23 @@ class CalendarForm extends Component {
 
 export default CalendarForm
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  width:100%;
+  padding: 0px 15px;
+  background-color: rgba(0,0,0,0.05);
+`
+
+const StyledForm = styled.form`
+
+`
+
+const Labels = styled.span`
+  font-weight: bold;
+`
+
+const SubmitError = styled.div`
+  color: white;
+  margin: 20px 0px;
+  background-color: #db2525;
+  padding: 10px;
+`
--- src/modules/Shared/Forms/input.js
+++ src/modules/Shared/Forms/input.js
@@ -2,11 +2,32 @@ import React from 'react'
 import styled from 'styled-components'
 
 const renderInput = ({input, meta, name, label = ''}) => (
-  <div>
-    <label>{label}</label>
-    <input {...input} type="text" placeholder={label || 'Enter value...'}/>
-    {meta.error && meta.touched && <span>{meta.error}</span>}
-  </div>
+  <InputWrap>
+    <Label>{label}</Label>
+    <Input error={meta.error && meta.touched} {...input} type="text" placeholder={label || 'Enter value...'}/>
+    {meta.error && meta.touched && <Error>{meta.error}</Error>}
+  </InputWrap>
 )
 
 export default renderInput
+
+const InputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`
+
+const Label = styled.label`
+  font-weight: bold;
+
+`
+
+const Input = styled.input`
+  border: 1px solid ${({error}) => error ? '#db2525' : '#6E9ECF' };
+  padding: 4px;
+  margin: 5px 0px;
+`
+
+const Error = styled.span`
+  color: #db2525;
+`
